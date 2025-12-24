0dd67496bc8512d8244d37f9cfdf78eecb7f9aa5
skeleton111222 <prasannashakya111@gmail.com>
2025-12-24 22:38:42 +0545

    Added files to Github

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Calculation
+{
+    static void Main(string[] args)
+    {
+        if (args.Length >=3)
+        {
+            try
+            {
+                int a = int.Parse(args[0]);
+                int b = int.Parse(args[1]);
+                string op = args[2];
+
+                switch (op)
+                {
+                    case "+":
+                        Console.WriteLine(a + b);
+                        break;
+                    case "-":
+                        Console.WriteLine(a - b);
+                        break;
+                    case "*":
+                        Console.WriteLine(a * b);
+                        break;
+                    case "/":
+                        try
+                        {
+                            Console.WriteLine(a/b);
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            Console.WriteLine("Cannot divide by zero.");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown operator.");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: " + ex.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Not enough arguments were provided.");
+        }
+    }
+}
